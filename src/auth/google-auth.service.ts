237bf467b22dcf5e8fc20fb2385b2c6eb07c52c9
anand221986 <anandgupta224@gmail.com>
2026-02-06 18:22:10 +0530

    push all codes

---
+++ src/auth/google-auth.service.ts
@@ -0,0 +1,17 @@
+// google-auth.service.ts
+import { Injectable } from '@nestjs/common';
+import { OAuth2Client } from 'google-auth-library';
+
+@Injectable()
+export class GoogleAuthService {
+  private client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+  async verifyToken(token: string) {
+    const ticket = await this.client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    return ticket.getPayload();
+  }
+}
